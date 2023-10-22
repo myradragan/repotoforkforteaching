58771c88e27b3c245fcca4d7f284fb31f8f3ece1
Linda <linda.gottfried@hotmail.com>
2023-10-22 19:42:27 +0200

    I tried c = a + b, but it didn't work

--- myhomeworkLG.py
+++ myhomeworkLG.py
@@ -1,5 +1,10 @@
 print ("Do I get a Sehr Gut now?")
 print ("Hello Again")
 print ("I'm not sure what else I can do except to pring sth")
+var1 = 432
+var2 = 234
+resultA = (var1 + var2)
+#I tried, but I don't get it
+# c=a+b also didn't work
 
     
